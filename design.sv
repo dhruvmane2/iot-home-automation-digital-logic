db41b648d306b2874025e9363b1581bb4373c95a
Dvm23 <dhruvmane33145@gmail.com>
2026-08-27 09:20:53 +0530

    Add home automation design code

--- design.sv
+++ design.sv
@@ -1,1 +1,19 @@
+`timescale 1ns/1ps
 
+module home_automation (
+    input logic ldr,
+    input logic pir,
+    input logic temp_high,
+    input logic gas_detected,
+    input logic remote_cmd,
+
+    output logic light,
+    output logic fan,
+    output logic alarm
+);
+
+    assign light = (ldr & pir) | remote_cmd;
+    assign fan = temp_high | remote_cmd;
+    assign alarm = gas_detected | pir;
+
+endmodule
